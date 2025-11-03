45bbba593056900969edac031823a9af07f2334e
Krish <krishverma837@gmail.com>
2025-11-03 21:23:45 +0530

    initial commit

---
+++ src/test/java/com/ghibli/ghibli_service/GhibliServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghibli.ghibli_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhibliServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
